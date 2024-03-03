d4e43d291d1f3ef17a58d3492162c97a399254d4
maheshkumawat274 <maheshkumar006376@gmail.com>
2024-03-03 12:20:50 +0530

    Webbuiler Assignment

--- src/components/about/Webbuilder.jsx
+++ src/components/about/Webbuilder.jsx
@@ -5,8 +5,8 @@ function Webbuilder({Webbuilder}) {
   return (
     <>
       <div className='row'>
-        {Webbuilder.map((item,i)=><div className='col-lg-4 col-md-6 col-sm-12'>
-        <AboutContent4 key={i} image={item.img} btn1 = {item.btn1} btn2 = {item.btn2} text1 ={item.text1} text2={item.text2} old_price = {item.old_price} new_price = {item.new_price} offer = {item.offer} btn3 = {item.btn3}
+        {Webbuilder.map((item,i)=><div key={i} className='col-lg-4 col-md-6 col-sm-12'>
+        <AboutContent4  image={item.img} btn1 = {item.btn1} btn2 = {item.btn2} text1 ={item.text1} text2={item.text2} old_price = {item.old_price} new_price = {item.new_price} offer = {item.offer} btn3 = {item.btn3}
         />
         </div>)}
       </div>
